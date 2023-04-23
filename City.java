0674653bc133be8b239478660b8b607cbf2fd064
Robert Turner <robturneris@gmail.com>
2023-04-22 22:36:24 -0400

    add City class with overloaded constructor

--- City.java
+++ City.java
@@ -1,1 +1,14 @@
 // Implement your City class in this file
+class City {
+  private String name = "";
+  private String state = "";
+  private double highTemp = 0.0;
+  private double lowTemp = 0.0;
+
+  public City(String name, String state, double highTemp, double lowTemp) {
+    this.name = name;
+    this.state = state;
+    this.highTemp = highTemp;
+    this.lowTemp = lowTemp;
+  }
+}
