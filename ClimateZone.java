4992931a4e19f057989c64960293432a9e2e228f
Robert Turner <robturneris@gmail.com>
2023-04-22 23:09:15 -0400

    add ClimateZone class with constructors

--- ClimateZone.java
+++ ClimateZone.java
@@ -1,1 +1,16 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // Implement your ClimateZone class in this file
+class ClimateZone {
+  private ArrayList<City> cityList = null;
+
+  public ClimateZone() {
+    this.cityList = new ArrayList<City>();
+  }
+
+  public ClimateZone(String fileName) throws IOException {
+
+  }
+}
