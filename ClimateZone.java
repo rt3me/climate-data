6296b2903e959c70d2836cea209e9527221106b3
Robert Turner <robturneris@gmail.com>
2023-04-22 23:17:34 -0400

    add read in data from file

--- ClimateZone.java
+++ ClimateZone.java
@@ -11,6 +11,24 @@ class ClimateZone {
   }
 
   public ClimateZone(String fileName) throws IOException {
+    this.cityList = new ArrayList<City>();
 
+    Scanner myFileReader = new Scanner(new FileInputStream(fileName));
+
+    while (myFileReader.hasNext()) {
+      String name = myFileReader.next();
+      String state = myFileReader.next();
+      double highTemp = myFileReader.nextDouble();
+      double lowTemp = myFileReader.nextDouble();
+
+      this.addCity(name, state, highTemp, lowTemp);
+    }
+
+    myFileReader.close();
+  }
+
+  public void addCity(String name, String state, double highTemp, double lowTemp) {
+    City newCity = new City(name, state, highTemp, lowTemp);
+    cityList.add(newCity);
   }
 }
