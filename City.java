bf453972d26d89d41024dd825432e6e1332f7013
Robert Turner <robturneris@gmail.com>
2023-04-22 22:50:26 -0400

    add methods to City class

--- City.java
+++ City.java
@@ -11,4 +11,32 @@ class City {
     this.highTemp = highTemp;
     this.lowTemp = lowTemp;
   }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getState() {
+    return this.state;
+  }
+
+  public double getHighTemp() {
+    return this.highTemp;
+  }
+
+  public void setHighTemp(double highTemp) {
+    this.highTemp = highTemp;
+  }
+
+  public double getLowTemp() {
+    return this.lowTemp;
+  }
+
+  public void setLowTemp(double lowTemp) {
+    this.lowTemp = lowTemp;
+  }
+
+  public void printInfo() {
+    System.out.printf("%s, %s (High = %.2f, Low = %.2f)", this.name, this.state, this.highTemp, this.lowTemp);
+  }
 }
